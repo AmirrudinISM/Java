fba761101cfd4341dfcd3cfd37b8a53a0a4ec170
Amirrudin Ismail bin Adlan Rahim <amirrudin_ism@outlook.my>
2020-02-20 16:15:37 +0800

    starting files

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld { 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]){ 
+        System.out.println("Hello, World");
+        System.out.print(summation(2,3)); 
+    }
+
+    public int summation(int num1, int num2){
+    	return num1+num2;
+    } 
+}
