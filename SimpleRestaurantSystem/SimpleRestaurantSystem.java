3c55d2e7cd2761e2797f823caeb4f8bff0c95cf3
Amirrudin Ismail bin Adlan Rahim <amirrudin_ism@outlook.my>
2020-03-22 22:44:50 +0800

    created a receipt that displays food ordered and amount next to it as well as formatted the price to 2 decimal places

--- SimpleRestaurantSystem/SimpleRestaurantSystem.java
+++ SimpleRestaurantSystem/SimpleRestaurantSystem.java
@@ -1,7 +1,11 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.text.DecimalFormat; 
 
 class SimpleRestaurantSystem{ 
 
+    private static DecimalFormat df = new DecimalFormat("0.00");
+
+    //DECISION
     public static boolean paid(double totalPrice, double inPrice){
     	if (inPrice >= totalPrice){
     		return true;
@@ -9,7 +13,8 @@ class SimpleRestaurantSystem{
     	else{
     		return false;
     	}
-    }  
+    }
+
     public static void main(String args[]){ 
 
         String foodName[] = new String[4]; 
@@ -24,10 +29,19 @@ class SimpleRestaurantSystem{
         price[2] = 6.5; 
         price[3] = 7.5; 
 
+
+        //output
+        String foodList = "";
+        //output
         double totalPrice = 0;
-        double paidPrice = 0;
+        //output
         boolean payStat = false; 
+        
+        //input
         int quant = 0; 
+        //input
+        double paidPrice = 0;
+        //input
         int sel = 0; 
 
         Scanner myIn = new Scanner(System.in); 
@@ -41,9 +55,11 @@ class SimpleRestaurantSystem{
              
         	System.out.println("===========MAIN COURSES===============");
             for (int i = 1; i <= 4; i++){ 
-                System.out.println(i+". "+foodName[i-1]+" | "+price[i-1]); 
+                System.out.println(i+". "+foodName[i-1]+" | "+df.format(price[i-1])); 
             } 
-            System.out.println("++++++++TOTAL PRICE: "+ totalPrice+"+++++++++++");
+            System.out.println("-------------YOUR ORDER-------------");
+            System.out.println(foodList);
+            System.out.println("++++++++TOTAL PRICE: "+ df.format(totalPrice)+"+++++++++++");
             System.out.println("5. Make payment");
             System.out.println("6. Cancel & Exit System");
 
@@ -51,40 +67,46 @@ class SimpleRestaurantSystem{
 
             sel = myIn.nextInt(); 
 
+            //DECISION
             switch(sel){ 
 
                 case 1: 
                     System.out.println(foodName[0]+" selected. Enter quantity:"); 
-                    quant = myIn.nextInt(); 
+                    quant = myIn.nextInt();
+                    foodList += foodName[0]+ ", " + quant + "\n";
                     totalPrice += price[0]*quant; 
                     break; 
                 case 2: 
                     System.out.println(foodName[1]+" selected. Enter quantity:"); 
-                    quant = myIn.nextInt();                   
+                    quant = myIn.nextInt();
+                    foodList += foodName[1]+ ", " + quant + "\n";                   
                     totalPrice += price[1]*quant; 
                     break; 
 
                 case 3:
                    	System.out.println(foodName[2]+" selected. Enter quantity:"); 
                     quant = myIn.nextInt();
+                    foodList += foodName[2]+ ", " + quant + "\n";
                     totalPrice += price[2]*quant; 
                     break; 
 
                 case 4: 
                     System.out.println(foodName[3]+" selected. Enter quantity:"); 
-                    quant = myIn.nextInt(); 
+                    quant = myIn.nextInt();
+                    foodList += foodName[3]+ ", " + quant + "\n"; 
                     totalPrice += price[3]*quant; 
                     break; 
 
-                case 5: 
+                case 5:
+                    //DECISION 
                     if(totalPrice > 0){
                     	while(payStat != true){
                     		System.out.println("Insert payment:");
                     		paidPrice += (myIn.nextDouble());
                     		payStat = paid(totalPrice,paidPrice);
                     		if (payStat == false){
-                    			System.out.println("Current paid amount:"+ paidPrice);
-                    			System.out.println("Remaining amount: "+ (totalPrice-paidPrice)+"\nPlease insert more cash");	
+                    			System.out.println("Current paid amount:"+ df.format(paidPrice));
+                    			System.out.println("Remaining amount: "+ df.format(totalPrice-paidPrice)+"\nPlease insert more cash");	
                     		}
                     	}
                     	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
