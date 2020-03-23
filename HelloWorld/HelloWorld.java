efbd51a1cb57f44f0d8acd2b4da68f6b552b01ef
AmirrudinISM <amirrudin_ism@outlook.my>
2020-03-23 15:15:16 +0800

    fixed summation function

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,4 +1,9 @@
-class HelloWorld { 
+class HelloWorld {
+    
+    public static int summation(int num1, int num2){
+    	return num1+num2;
+    } 
+    
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]){ 
@@ -6,7 +11,5 @@ class HelloWorld {
         System.out.print(summation(2,3)); 
     }
 
-    public int summation(int num1, int num2){
-    	return num1+num2;
-    } 
+    
 }
