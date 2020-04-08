f5108afc423a072f26ccda3084a1b9889a5a842b
AmirrudinISM <amirrudin_ism@outlook.my>
2020-04-08 19:13:10 +0800

    completed tutorial 3 q1 & q2

---
+++ Tutorial3/Question2.java
@@ -0,0 +1,24 @@
+class Question2{
+	public static void main(String[] args) {
+		int i = 1;
+
+		//while
+		while (i <= 30){
+			System.out.println(i);
+			i++;
+		}
+
+		//for
+		for(i = 1; i <= 30; i++){
+			System.out.println(i);
+		}
+
+		i = 1;
+		//do-while
+		do{
+			System.out.println(i);
+			i++;
+		}while(i <= 30);
+
+	}
+}
